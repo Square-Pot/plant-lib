63d6630638a468474963a41189140c073e2a3352
spalk <d.natxa@gmail.com>
2023-02-01 15:06:18 +0300

    upd: front

---
+++ frontend/plantlib/src/components/PlantScanResult.vue
@@ -0,0 +1,62 @@
+<script setup>
+defineProps({
+    plant: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title fw-light">
+                    <template v-if="plant.field_number"><span class="badge bg-secondary">{{ plant.field_number }}</span> </template>
+                    <span class="fst-italic fw-normal">  {{ '  ' + plant.genus.charAt(0).toUpperCase() + plant.genus.slice(1) }}</span>
+                    <template v-if="plant.species">  sp. <span class="fst-italic fw-normal">{{ plant.species }}</span></template>
+                    <template v-if="plant.subspecies">  ssp. <span class="fst-italic fw-normal">{{ plant.subspecies }}</span></template>
+                    <template v-if="plant.variety">  var. <span class="fst-italic fw-normal">{{ plant.variety }}</span></template>
+                    <template v-if="plant.cultivar">  cv. '<span class="fst-italic fw-normal">{{ plant.cultivar }}</span>'</template>
+                  </h5>
+                  <table class="table" style="font-size:0.80em;">
+                    <tbody>
+                      <tr>
+                        <th scope="row">Age:</th>
+                        <td>{{ plant.date_purchase }}</td>
+                      </tr>
+                      <tr  v-if="plant.source">
+                        <th scope="row">Source:</th>
+                        <td>{{ plant.source }}</td>
+                      </tr>
+                      <tr v-if="plant.form">
+                        <th scope="row">Form:</th>
+                        <td>{{ plant.form }}</td>
+                      </tr>
+                      <tr v-if="plant.affinity">
+                        <th scope="row">Affinity:</th>
+                        <td>{{ plant.affinity }}</td>
+                      </tr>
+                      <tr v-if="plant.ex">
+                        <th scope="row">Ex:</th>
+                        <td>{{ plant.ex }}</td>
+                      </tr>
+                      <tr v-if="plant.geography">
+                        <th scope="row">Geography:</th>
+                        <td>{{ plant.geography }}</td>
+                      </tr>
+                      <tr v-if="plant.price">
+                        <th scope="row">Price:</th>
+                        <td>{{ plant.price }}</td>
+                      </tr>
+                      <tr v-if="plant.user_number">
+                        <th scope="row">Custom number:</th>
+                        <td>{{ plant.user_number }}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <p class="card-text" v-if="plant.info">{{ plant.info }}</p>
+                  <p class="card-text" v-if="plant.description">{{ plant.description }}</p>
+                  <p class="card-footer text-end"><small class="text-muted">UID: {{ plant.uid }}</small></p>
+                </div>
+              </div>
+</template>
--- frontend/plantlib/src/components/Scan.vue
+++ frontend/plantlib/src/components/Scan.vue
@@ -1,5 +1,6 @@
 <script setup>
-import QRCodeScanner from './QRCodeScanner.vue'
+import QRCodeScanner from './QRCodeScanner.vue';
+import PlantScanResult from './PlantScanResult.vue';
 </script>
 
 <script>
@@ -32,14 +33,13 @@ import QRCodeScanner from './QRCodeScanner.vue'
         // console.log(decodedText);
         // console.log(decodedResult);
         this.getData(decodedText);
-
       },
       async getData(uid) {
         try {
             const response = await axios.get('https://galangal.ru:8000/api/plant/' + uid);
             this.plant = response.data; 
             if (!this.multipleMode){
-              this.stopScanning = !this.stopScanning;
+              stopScanning = !stopScanning;
               this.show_scanner = false;
               this.show_result = true;
             } else {
@@ -98,57 +98,7 @@ import QRCodeScanner from './QRCodeScanner.vue'
         <div class="row justify-content-center my-3">
           <div class="col-10 d-flex justify-content-center">
 
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title fw-light">
-                    <template v-if="plant.field_number"><span class="badge bg-secondary">{{ plant.field_number }}</span> </template>
-                    <span class="fst-italic fw-normal">  {{ '  ' + plant.genus.charAt(0).toUpperCase() + plant.genus.slice(1) }}</span>
-                    <template v-if="plant.species">  sp. <span class="fst-italic fw-normal">{{ plant.species }}</span></template>
-                    <template v-if="plant.subspecies">  ssp. <span class="fst-italic fw-normal">{{ plant.subspecies }}</span></template>
-                    <template v-if="plant.variety">  var. <span class="fst-italic fw-normal">{{ plant.variety }}</span></template>
-                    <template v-if="plant.cultivar">  cv. '<span class="fst-italic fw-normal">{{ plant.cultivar }}</span>'</template>
-                  </h5>
-                  <table class="table" style="font-size:0.80em;">
-                    <tbody>
-                      <tr>
-                        <th scope="row">Age:</th>
-                        <td>{{ plant.date_purchase }}</td>
-                      </tr>
-                      <tr  v-if="plant.source">
-                        <th scope="row">Source:</th>
-                        <td>{{ plant.source }}</td>
-                      </tr>
-                      <tr v-if="plant.form">
-                        <th scope="row">Form:</th>
-                        <td>{{ plant.form }}</td>
-                      </tr>
-                      <tr v-if="plant.affinity">
-                        <th scope="row">Affinity:</th>
-                        <td>{{ plant.affinity }}</td>
-                      </tr>
-                      <tr v-if="plant.ex">
-                        <th scope="row">Ex:</th>
-                        <td>{{ plant.ex }}</td>
-                      </tr>
-                      <tr v-if="plant.geography">
-                        <th scope="row">Geography:</th>
-                        <td>{{ plant.geography }}</td>
-                      </tr>
-                      <tr v-if="plant.price">
-                        <th scope="row">Price:</th>
-                        <td>{{ plant.price }}</td>
-                      </tr>
-                      <tr v-if="plant.user_number">
-                        <th scope="row">Custom number:</th>
-                        <td>{{ plant.user_number }}</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                  <p class="card-text" v-if="plant.info">{{ plant.info }}</p>
-                  <p class="card-text" v-if="plant.description">{{ plant.description }}</p>
-                  <p class="card-footer text-end"><small class="text-muted">UID: {{ plant.uid }}</small></p>
-                </div>
-              </div>
+              <PlantScanResult :plant="plant" />
                 
           </div>
         </div>
